99b01e5f3c884d79c1e8c46d65c188efe9701e37
abbasalirezaei <abbasalirezaei.79@gmail.com>
2023-10-31 11:22:47 +0330

    add gob

--- src/components/sections/Sidebar/Sidebar.jsx
+++ src/components/sections/Sidebar/Sidebar.jsx
@@ -63,10 +63,10 @@ const Sidebar = () => {
       <div className="text-center">
         <h1 className="text-xl text-gray-800 font-bold mb-1">Abbasali Rezaei</h1>
         <p className="text-sm text-gray-400 mb-3">
-          Frontend Web Developer at
-          <a href="#0" className="text-purple-600 pl-1">
+          Full Stack Web Developer
+          {/* <a href="#0" className="text-purple-600 pl-1">
             Abc Company
-          </a>
+          </a> */}
         </p>
         <a
           href="#0"
